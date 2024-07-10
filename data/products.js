4c2fa77d1ac8ca6f7bffd3a974a727cd3168d80a
Gustavo Campos <guustavo1619@gmail.com>
2024-07-10 14:48:27 -0300

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -102,10 +102,14 @@ export function loadProductsFetch(){
       return new Product(productDetails);
     });
     console.log('load products');
+  }).catch(() => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
 }
+
+
 /*
 loadProductsFetch().then(() => {
   console.log('next step')
@@ -127,6 +131,11 @@ export function loadProducts(fun){
 
     fun();
   });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
