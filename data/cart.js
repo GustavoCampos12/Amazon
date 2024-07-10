4bd6c7abeaa2ecbad126c8adb314ff743601f312
Gustavo Campos <guustavo1619@gmail.com>
2024-07-10 19:57:26 -0300

    When clicking Add to Cart it appers Added above the button

--- data/cart.js
+++ data/cart.js
@@ -47,7 +47,23 @@ export function addToCart(productId){
         });
     }
 
-   
+    const addedMessageTimeouts = {};
+
+    const addedMessage = document.querySelector(
+      `.js-added-to-cart-${productId}`
+    );
+    addedMessage.classList.add('added-to-cart-visible');
+    
+    const previousTimeoutId = addedMessageTimeouts[productId];
+    if (previousTimeoutId) {
+      clearTimeout(previousTimeoutId);
+    }
+
+    const timeoutId = setTimeout(() => {
+      addedMessage.classList.remove('added-to-cart-visible');
+    }, 2000);
+
+    addedMessageTimeouts[productId] = timeoutId;
 
     saveToStorage();
 };
